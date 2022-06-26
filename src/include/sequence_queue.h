0bfd884b0a2404edb9b6fd7f2d2b787c070bdbe6
Ilya <litik@arobosys.com>
2022-06-26 18:18:58 +0300

    Implement minimal functionality

---
+++ src/include/sequence_queue.h
@@ -0,0 +1,116 @@
+//
+// Created by RunoVj on 5/28/2022.
+//
+#pragma once
+#include "allocator.h"
+#include <iostream>
+
+
+template<typename T, typename Allocator = std::allocator<T>>
+class SequenceQueue
+{
+public:
+    SequenceQueue() :
+        _size{0},
+        _head{nullptr},
+        _tail{nullptr}
+    {}
+
+    ~SequenceQueue()
+    {
+        _allocator.deallocate(_tail, _size);
+    }
+
+    bool is_empty()
+    {
+        return _head == nullptr && _tail == nullptr;
+    }
+
+    bool is_full()
+    {
+        if (is_empty()) {
+            return true;
+        }
+        return static_cast<size_t>((_head - _tail)) == _size - 1;
+    }
+
+    void push_back(T &obj) {
+        if (is_full()) {
+            _tail = _allocator.allocate(1);
+            ++_size;
+            _allocator.construct(_tail, obj);
+            if (_head == nullptr) {
+                _head = _tail;
+            }
+        }
+        else {
+            *(--_tail) = obj;
+        }
+    }
+
+    T pop_front()
+    {
+        try {
+            if (is_empty()) {
+                throw std::range_error("Queue is empty! ");
+            }
+            auto ret_val = *_head;
+            if (_head == _tail) {
+                _head = _tail = nullptr;
+            }
+            --_head;
+            return ret_val;
+        }
+        catch (const char* str)
+        {
+            std::cout << std::endl << str << std::endl;
+        }
+        return T{};
+    }
+
+    T back()
+    {
+        if (_tail != nullptr) {
+            return *_tail;
+        }
+        else {
+            return T{};
+        }
+    }
+
+    T front()
+    {
+        if (_head != nullptr) {
+            return *_head;
+        }
+        else {
+            return T{};
+        }
+    }
+
+//    friend std::ostream& operator<<(std::ostream& os, const SequenceQueue &obj)
+//    {
+//        for (auto i = obj.tail; i < obj.head; ++i) {
+//            os << obj.arr[i] << " ";
+//        }
+//        os << std::endl;
+//        return os;
+//    }
+//
+//    friend std::istream& operator>>(std::istream& is, SequenceQueue &obj)
+//    {
+//        T temp=0;
+//        while ( !obj.is_full() && (is >> temp))
+//            obj.push(temp);
+//        std::cout << "Queue is full!" << std::endl;
+//        return is;
+//    }
+
+private:
+    Allocator _allocator;
+    size_t _size;
+    T* _head;
+    T* _tail;
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,74 @@
 #include "allocator.h"
 #include <vector>
 #include <map>
+#include "factorial.h"
+#include "sequence_queue.h"
 
-/**
-
-- создание экземпляра своего контейнера для хранения значений типа int
-- заполнение 10 элементами от 0 до 9
-- создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
-ограниченным 10 элементами
-- заполнение 10 элементами от 0 до 9
-- вывод на экран всех значений, хранящихся в контейнере
- */
 
 int main()
 {
-    /// создание экземпляра std::map<int, int>
-    std::map<int, int> m;
-    /// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
-    for (int i = 0; i < 10; ++i) {
-        m.insert(std::pair<int, int>(i, factorial(i)));
+    {
+        /// создание экземпляра std::map<int, int>
+        std::map<int, int> m;
+        /// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+        for (int i = 0; i < 10; ++i) {
+            m.insert(std::pair<int, int>(i, factorial(i)));
+        }
+
+        /// это надо???
+        for (const auto& elem : m) {
+            std::cout << elem.first << ": " << elem.second << "  ";
+        }
+        std::cout << std::endl;
     }
 
-    /// это надо???
-    for(const auto& elem : m) {
-        std::cout << elem.first << ": " << elem.second << "  ";
+    {
+        /// создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
+        std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 10>> custom_allocator_map;
+
+        /// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+        for (int i = 0; i < 10; ++i) {
+            custom_allocator_map.insert(std::pair<int, int>(i, factorial(i)));
+        }
+        /// вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
+        for(const auto& elem : custom_allocator_map) {
+            std::cout << elem.first << ": " << elem.second << "  ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 
-    /// создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
-    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 10>> custom_allocator_map;
+    {
+        /// создание экземпляра своего контейнера для хранения значений типа int
+        SequenceQueue<int, MyAllocator<int, 10>> my_queue;
 
-    /// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
-    for (int i = 0; i < 10; ++i) {
-        custom_allocator_map.insert(std::pair<int, int>(i, factorial(i)));
+        /// заполнение 10 элементами от 0 до 9
+        for (int i = 0; i < 10; ++i) {
+            my_queue.push_back(i);
+        }
+
+        /// это надо?
+        for (int i = 0; i < 10; ++i) {
+            std::cout << my_queue.pop_front() << " ";
+        }
+        std::cout << std::endl;
     }
-    /// вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
-    for(const auto& elem : custom_allocator_map) {
-        std::cout << elem.first << ": " << elem.second << "  ";
+
+
+    /// создание экземпляра своего контейнера для хранения значений типа int
+    /// с новым аллокатором,ограниченным 10 элементами
+    {
+        SequenceQueue<int, MyAllocator<int, 10>> my_queue_with_my_allocator;
+
+        /// заполнение 10 элементами от 0 до 9
+        for (int i = 0; i < 10; ++i) {
+            my_queue_with_my_allocator.push_back(i);
+        }
+
+        /// вывод на экран всех значений, хранящихся в контейнере
+        for (int i = 0; i < 10; ++i) {
+            std::cout << my_queue_with_my_allocator.pop_front() << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-
-//    auto v = std::vector<int, my_allocator<int>>{};
-//    // v.reserve(5);
-//    for (int i = 0; i < 6; ++i) {
-//        std::cout << "vector size = " << v.size() << std::endl;
-//        v.emplace_back(i);
-//        std::cout << std::endl;
-//    }
-//
-//    auto m = std::map<
-//        int,
-//        float,
-//        std::less<int>,
-//        my_allocator<
-//            std::pair<
-//                const int, float
-//            >
-//        >
-//    >{};
-//
-//    for (int i = 0; i < 1; ++i) {
-//        m[i] = static_cast<float>(i);
-//    }
-
     return 0;
 }
